63bcbe1848742b581d2de6556e3690828070cd71
Camal Cakar <camal_cakar@yahoo.de>
2020-03-29 17:46:12 +0200

    Add health endpoint handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/midnightrun/k8s-health-and-readiness
 
 go 1.14
+
+require github.com/sirupsen/logrus v1.5.0
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Hello K8S!")
+	log.WithTime(time.Now()).Info("Starting up process")
+	rand.Seed(time.Now().Unix())
+
+	http.HandleFunc("/healtz", handleHealthz)
+	http.HandleFunc("/readiness", handleReadiness)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.WithTime(time.Now()).Fatalf("Process terminated: %v", err)
+	}
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	n := rand.Intn(3)
+
+	if n != 0 {
+		log.WithTime(time.Now()).Info("Imitate process problem")
+		time.Sleep(time.Second * 5)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+
 }
